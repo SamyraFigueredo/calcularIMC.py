b09266faf85803411c729f5afe044f33f93f6157
SamyraFigueredo <104530046+SamyraFigueredo@users.noreply.github.com>
2022-09-11 11:36:15 -0300

    Create calcularIMC.py

---
+++ calcularIMC.py
@@ -0,0 +1,11 @@
+peso = float(input("peso: ")) # peso em kg
+altura = float(input("altura: ")) # altura em M
+
+IMC = peso/ (altura**2)
+
+print("seu IMC é de: ", IMC)
+
+print ("{:.2f}" .format(IMC))
+
+
+
